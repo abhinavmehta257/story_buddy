f6fc70d692ba10fd66c9b0fa13b0dac0a4b6d202
abhinavmehta257 <61967812+abhinavmehta257@users.noreply.github.com>
2024-03-09 19:56:57 +0530

    install button added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import InstallPWAbutton from "../components/blocks/InstallPWAbutton";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>{" "}
+      {/* <InstallPWAbutton /> */}
+    </>
+  );
 }
